4f602a61eb343821c65f539dacd0a93150deff58
Raj <Rajjani1009@icloud.com>
2024-06-28 23:49:42 +0530

    Update All_in_One.js

--- Java_Script/datatypes&varaibles/string/All_in_One.js
+++ Java_Script/datatypes&varaibles/string/All_in_One.js
@@ -1,30 +1,17 @@
+// function 1
 function concatenation(first_name,last_name)
 {
     return first_name + last_name;
 }
 console.log(concatenation('raj','jani'));
-
+// function 2
 function string_length(str)
 {
     return str.length;
 }
 console.log("String Length is "+" "+string_length("RAJ"));
 
-
-// function accessing_element(mystr)
-// {
-//     return mystr.charAt(0);
-// }
-// console.log("RAJ"+" "+ "First Element is "+ " "+accessing_element("RAJ"));
-
-
-// function last_element(mystr_last)
-// {
-//     return mystr_last.charAt(mystr_last.length-1);
-// }
-
-// console.log("RAJ"+" "+"Last Element is "+" "+last_element("RAJ"))
-
+// function 3
 function access_element(string)
 {
     let first_element = string.charAt(0);
@@ -32,11 +19,10 @@ function access_element(string)
     return {first_element,last_element};
 }
 let passing_value = access_element("RAJ");
-
-
 console.log("First Element is "+" "+passing_value.first_element);
 console.log("Last Element is"+" "+passing_value.last_element);
 
+// function 4
 function substring_extract(str, start, end)
 {
     return str.substring(start,end);
@@ -44,7 +30,7 @@ function substring_extract(str, start, end)
 }
 console.log(substring_extract("RAJ JANI",2,5));
 
-
+// function 5
 function replace_string(sentance)
 {
  
@@ -56,7 +42,7 @@ function replace_string(sentance)
 }
 replace_string("I AM RAJ");
 
-
+// function 6
 function split_string(split_str)
 {
     let new_splited = split_str.split(" ");
@@ -65,7 +51,7 @@ function split_string(split_str)
 }
 split_string("I AM RAJ");   
 
-
+// function 7
 function string_to_case(st_case)
 {
     let upper_case = st_case.toUpperCase();
@@ -76,22 +62,22 @@ function string_to_case(st_case)
 }
 string_to_case("RaJ")
 
+// function 8
 function string_includes(str,sub_st)
 {
     return str.includes(sub_st);
    
 }
-
 console.log(string_includes("I am boy","boy"));
 
+// function 9
 function trim_space(str)
 {
     return str.trim();
 }
 console.log(trim_space(" RAJJANI"));
 
-
-
+// function 10
 function repe(str,times)
 {
     return str.repeat(times);
