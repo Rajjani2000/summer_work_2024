a230292408986ee277b72eaa987a4e9bc0860b17
Raj <Rajjani1009@icloud.com>
2024-06-30 12:00:50 +0530

    Create String_manipulation.js

---
+++ Java_Script/datatypes&varaibles/String_manipulation.js
@@ -0,0 +1,14 @@
+var mystr = 'I am an "RAJ"';
+console.log(mystr);
+
+
+var a = "I am";
+var b = "Raj";
+var c = a +" "+ b;
+console.log(c)
+
+
+var anAdjective = "Easy";
+var Sentance = "Learning Javascipt";
+Sentance += anAdjective;
+console.log(Sentance);
